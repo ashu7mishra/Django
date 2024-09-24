370eaf308aa4be3557ff3374ed9ac521787792a4
ashu7mishra <ashu7mishra@gmail.com>
2024-09-24 21:22:54 +0530

    Django project setup

--- instaclone/users/views.py
+++ instaclone/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    # message = f"{request.method} -----> {request.path_info}"
+
+    message = "I don't know about your favourite color."
+
+    my_favourite_color = request.GET['my_favourite_color']
+
+    if my_favourite_color == 'blue':
+        message = "The SKY is blue."
+    elif my_favourite_color == 'yellow':
+        message = "The SUN is Yellow."
+
+    return HttpResponse(message)
