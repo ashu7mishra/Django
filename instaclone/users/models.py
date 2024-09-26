c642680fd278c10d4926ec509c5b8d608de1da82
ashu7mishra <ashu7mishra@gmail.com>
2024-09-26 11:04:48 +0530

    Django project continue

--- instaclone/users/models.py
+++ instaclone/users/models.py
@@ -6,25 +6,22 @@ class User(models.Model):
     name = models.CharField(max_length=255, null=False)
     email = models.EmailField(max_length=255, unique=True, null=False)
     phone_number = models.CharField(max_length=10, unique=True)
+    password = models.CharField(max_length=55, null=False)
     is_active = models.BooleanField(default=False)
 
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
 
-class UserProfile(TimeStamp):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False,
-                                related_name='profile')
+class UserProfile(models.Model):
 
-    profile_pic_url = models.ImageField(upload_to='profile_pic/', blank=True)
+    DEFAULT_PROFILE_PIC_URL = "https://my_website.com/placeholder.png"
 
+    profile_pic_url = models.CharField(max_length=255, default=DEFAULT_PROFILE_PIC_URL)
     bio = models.CharField(max_length=255, blank=True)
-    is_verified = models.BooleanField(default=True)
-
-    class Meta:
-        unique_together = ('from_user', 'to_user')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
 
--- instaclone/users/views.py
+++ instaclone/users/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 
 def index(request):
     # message = f"{request.method} -----> {request.path_info}"
 
-    message = "I don't know about your favourite color."
+    # message = "I don't know about your favourite color."
+    #
+    # my_favourite_color = request.GET['my_favourite_color']
+    #
+    # if my_favourite_color == 'blue':
+    #     message = "The SKY is blue."
+    # elif my_favourite_color == 'yellow':
+    #     message = "The SUN is Yellow."
 
-    my_favourite_color = request.GET['my_favourite_color']
+    # return HttpResponse(message)
 
-    if my_favourite_color == 'blue':
-        message = "The SKY is blue."
-    elif my_favourite_color == 'yellow':
-        message = "The SUN is Yellow."
+    user_count = User.objects.count()
 
-    return HttpResponse(message)
+    return render(request, 'users/index.html')
