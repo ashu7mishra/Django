77864df84c5f99551c2c6b5eb2af20e80e01d47b
ashu7mishra <ashu7mishra@gmail.com>
2025-03-09 17:37:29 +0530

    restructuring

--- Django/views/__init__.py
+++
@@ -1,1 +0,0 @@
-from .say_hello import say_hello, say_hello_with_name
--- Django/views/say_hello.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse, HttpRequest
-
-
-def say_hello(request):
-    return HttpResponse("Hello, world !!!")
-
-def say_hello_with_name(request: HttpRequest, name):
-    print(request.headers)
-    return HttpResponse("Hello, world !!! %s" % name)
