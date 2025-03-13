b0b1ea77d464255089f7466d18e56f5b17233ab2
ashu7mishra <ashu7mishra@gmail.com>
2025-03-13 08:18:25 +0530

    adding views

--- LoadBalancer/LoadBalancerService/LoadBalancer/views.py
+++ LoadBalancer/LoadBalancerService/LoadBalancer/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from models import Request, Server
+from services.loadBalancer import LoadBalancerSingleton
 
-# Create your views here.
+
+class RegisterServiceView(View):
+    def post(self, request):
+        ip_address = request.POST.get("ip_address")
+        port = request.POST.get('port')
+        server = Server.objects.create(i_address=ip_address, port=port)
+        return JsonResponse({"message": "Server registered", "server_id": server.id})
+
+
+class DistributeRequestView(View):
+    def get(self, request):
+        load_balancer =
