55aaa12fa3d695f0d9ed502b2099975c68c2712e
ashu7mishra <ashu7mishra@gmail.com>
2025-03-14 13:02:00 +0530

    adding student data

--- printMyName/getName/views.py
+++ printMyName/getName/views.py
@@ -1,6 +1,8 @@
 import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Student
 
 
@@ -8,15 +10,16 @@ def print_name(request, name):
     return HttpResponse(f"name: {name}")
 
 
+@csrf_exempt
 def add_student(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            student = Student.object.create(
-                name = data['name'],
-                student_class = data['student_class'],
-                age = data['age'],
-                address = data['address']
+            student = Student.objects.create(
+                name = data.get('name'),
+                student_class = data.get('student_class'),
+                age = data.get('age'),
+                address = data.get('address')
             )
             return JsonResponse({'message': 'student added successfully', 'student_id': student.id}, status=201)
         except Exception as e:
