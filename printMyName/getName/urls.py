4d9334e48a5737eccc311a698f215e4e8ca3cbeb
ashu7mishra <ashu7mishra@gmail.com>
2025-03-14 20:38:11 +0530

    updated project name

--- printMyName/getName/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import print_name, add_student
-
-urlpatterns = [
-    path('name/<str:name>/', print_name),
-    path('add_student/', add_student, name='add_student'),
-]
