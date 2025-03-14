f6e53de2ab997977fd83c92e1e043a8a278c35ca
ashu7mishra <ashu7mishra@gmail.com>
2025-03-14 23:46:34 +0530

    added model in cache management app

--- CacheManagementService/CacheManagementApp/models.py
+++ CacheManagementService/CacheManagementApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class CacheEntry(models.Model):
+    key = models.CharField(max_length=255, unique=True)
+    value = models.TextField()
+    frequency = models.IntegerField(default=0)  # used for LFU
+    last_accessed = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.key}: {self.value}"
---
+++ CacheManagementService/CacheManagementApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CacheView
+
+
+urlpattern = [
+    # path('', CacheView.as_view(), name='cache'),
+]
