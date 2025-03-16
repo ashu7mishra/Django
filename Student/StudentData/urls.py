c835a5112f67d96216058a901806b2e937e97f93
ashu7mishra <ashu7mishra@gmail.com>
2025-03-16 19:16:39 +0530

    restructuring

--- Student/StudentData/urls.py
+++ Student/StudentData/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import health, add_student, get_students, get_student_by_id, update_student, delete_student
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('health/', health),
     path('add_student/', add_student, name='add_student'),
     path('get_students/', get_students, name='get_students'),
