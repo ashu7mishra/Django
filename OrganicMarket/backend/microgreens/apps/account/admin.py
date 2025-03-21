b741bf926a581afb7a9c4678301d789e82515720
ashu7mishra <ashu7mishra@gmail.com>
2025-03-21 07:43:46 +0530

    admin register

--- OrganicMarket/backend/microgreens/apps/account/admin.py
+++ OrganicMarket/backend/microgreens/apps/account/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import CustomerUser
 
 
-admin.site.register(CustomerUser)
+# admin.site.register(CustomerUser)
+
+
+@admin.register(CustomerUser)
+class UserCustomer(admin.ModelAdmin):
+    fields = ["username", "phone", "address"]
--- OrganicMarket/backend/microgreens/apps/products/admin.py
+++ OrganicMarket/backend/microgreens/apps/products/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Category, Product
 
 
-admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Category)
+# admin.site.register(Product)
+
+@admin.register(Category)
+class UserCategory(admin.ModelAdmin):
+    fields = ["name"]
+
+
+@admin.register(Product)
+class UserProduct(admin.ModelAdmin):
+    fields = ["name", "price", "stock", "description"]
