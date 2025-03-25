148ed8e33b5b3c74cb28d0e3ed2aed4d7cbd259c
ashu7mishra <ashu7mishra@gmail.com>
2025-03-25 07:53:01 +0530

    default shipping address

--- Ecommerce/Ecommerce/models.py
+++ Ecommerce/Ecommerce/models.py
@@ -8,6 +8,12 @@ class User(models.Model):
     phone_number = models.DecimalField(max_digits=10, decimal_places=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    default_shipping_address = models.ForeignKey(
+        "ShippingAddress",
+        on_delete=models.DO_NOTHING,
+        null=True,
+        related_query_name="user_info"
+    )
 
     def __str__(self):
         return self.name
