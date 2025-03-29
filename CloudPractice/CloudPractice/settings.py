4b8560bd7895668e8df955d43c33a6a1ba945c1a
ashu7mishra <ashu7mishra@gmail.com>
2025-03-29 08:45:30 +0530

    cloud practice

--- CloudPractice/CloudPractice/settings.py
+++ CloudPractice/CloudPractice/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vaayo@g)gs8re16glo*@izu^p9gkyr=+5a22q7uc-x=2k!nh*3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.1.9"]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "CloudPractice",
 ]
 
 MIDDLEWARE = [
